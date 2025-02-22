d034afa1f8010cda495864107fd0666d9a6f8e3b
Tyson Do <doraeminemon@gmail.com>
2025-02-22 17:18:02 +0700

    Remove backend code.

--- main.go
+++ main.go
@@ -1,9 +1,7 @@
 package main
 
 import (
-	"database/sql"
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -50,34 +48,6 @@ func (h spaHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// initialize db
-	// dbName := "local.db"
-	// primaryUrl := "https://thuoclaodb-doraeminemon.turso.io"
-	// authToken := "eyJhbGciOiJFZERTQSIsInR5cCI6IkpXVCJ9.eyJleHAiOjE3NDAxMzYxMzEsImlhdCI6MTc0MDEzMjUzMSwiaWQiOiI5NDg4YzE0NS1kMDY3LTQwYWEtYThkOC0yMTAyYzY2ZGQ0OWIifQ.ubE-kcIlFEh-oLjxGtX6svx3BGSZwIyL3D9BVr3iKbdPRo3Y1r0tLZrpTitHNKQcVmZnSwA0AkVjMcZvoL_cAQ"
-
-	// dir, err := os.MkdirTemp("", "libsql-*")
-	// if err != nil {
-	// 	fmt.Println("Error creating temporary directory:", err)
-	// 	os.Exit(1)
-	// }
-	// defer os.RemoveAll(dir)
-
-	// dbPath := filepath.Join(dir, dbName)
-
-	// connector, err := libsql.NewEmbeddedReplicaConnector(dbPath, primaryUrl,
-	// 	libsql.WithAuthToken(authToken),
-	// )
-	// if err != nil {
-	// 	fmt.Println("Error creating connector:", err)
-	// 	os.Exit(1)
-	// }
-	// defer connector.Close()
-
-	// db := sql.OpenDB(connector)
-	// defer db.Close()
-
-	// queryProducts(db)
-
 	// initialize router
 	router := mux.NewRouter()
 
@@ -98,36 +68,4 @@ func main() {
 	}
 
 	log.Fatal(srv.ListenAndServe())
-}
-
-type Product struct {
-	ID   int
-	Name string
-}
-
-func queryProducts(db *sql.DB) {
-	rows, err := db.Query("SELECT id, name FROM products")
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "failed to execute query: %v", err)
-		os.Exit(1)
-	}
-	defer rows.Close()
-
-	var products []Product
-
-	for rows.Next() {
-		var product Product
-
-		if err := rows.Scan(&product.ID, &product.Name); err != nil {
-			fmt.Println("Error scanning row:", err)
-			return
-		}
-
-		products = append(products, product)
-		fmt.Println(product.ID, product.Name)
-	}
-
-	if err := rows.Err(); err != nil {
-		fmt.Println("Error during rows iteration:", err)
-	}
 }
