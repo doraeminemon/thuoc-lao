3d4625abe96ebf870ee3997fc64f4fbda903c734
Tyson Do <doraeminemon@gmail.com>
2025-02-27 23:03:58 +0700

    Comment out preline.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,10 @@
 module.exports = {
   content: [
     // './src/**/*.{html,js}',
-    'node_modules/preline/dist/*.js',
+    // 'node_modules/preline/dist/*.js',
   ],
   plugins: [
     // require('@tailwindcss/forms'),
-    require('preline/plugin'),
+    // require('preline/plugin'),
   ],
 }
